7f17b1012d8a77da43eaa7510e4d33709f404a06
hans <hans@fugal.net>
2006-01-21 13:36:31 -0700

    ladspa function signatures
    darcs-hash:20060121203631-28bda-aeedefb97a7659e08dbf20b79f17a563a8148675.gz

--- ladspa/haas-ladspa.c
+++ ladspa/haas-ladspa.c
@@ -5,25 +5,36 @@ extern "C" {
 #endif
 
     /* functions */
+    LADSPA_Handle haas_instantiate(const struct _LADSPA_Descriptor*, 
+	    unsigned long sr);
+    void haas_connect_port(LADSPA_Handle, unsigned long port, LADSPA_Data*);
+    void haas_activate(LADSPA_Handle);
+    void haas_run(LADSPA_Handle, unsigned long sample_count);
+    void haas_run_adding(LADSPA_Handle, unsigned long sample_count);
+    void haas_set_run_adding_gain(LADSPA_Handle, LADSPA_Data);
+    void haas_deactivate(LADSPA_Handle);
+    void haas_cleanup(LADSPA_Handle);
+
+    /* descriptor */
     static LADSPA_Descriptor descr = {
-	0x42,		// UniqueID
+	0x42,		// UniqueID TODO
 	"haas",		// Label
-	0,		// Properties
+	0,		// Properties TODO
 	"Haas Effect",	// Name
 	"Hans Fugal",	// Maker
 	"Copyright (C) 2005 Hans Fugal <hans@fugal.net>", // Copyright
 	4,		// PortCount
-	0,		// PortDescriptors
-	0,		// PortNames
-	0,		// PortRangeHints
-	0,		// ImplementationData
+	0,		// PortDescriptors TODO
+	0,		// PortNames TODO
+	0,		// PortRangeHints TODO
+	0,		// ImplementationData TODO
 	haas_instantiate,	// instantiate
 	haas_connect_port,	// connect_port
-	0,		// activate
+	0,		// activate XXX
 	haas_run,	// run
-	0,		// run_adding
-	0,		// set_run_adding_gain
-	0,		// deactivate
+	0,		// run_adding XXX
+	0,		// set_run_adding_gain XXX
+	0,		// deactivate XXX
 	haas_cleanup	// cleanup
     };
 
