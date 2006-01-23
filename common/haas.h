fa72b6c4cd105f1c3e39ec0b7724443c72120533
hans <hans@fugal.net>
2006-01-23 07:22:23 -0700

    haas.h
    darcs-hash:20060123142223-28bda-dce6ed0a8c2e1e70285e1ad94cf79a0cf8787849.gz

--- common/haas.h
+++ common/haas.h
@@ -3,4 +3,34 @@
 
 #include <SndObj/AudioDefs.h>
 
+class Haas
+{
+public:
+    /// Stereo input, stereo output.
+    void run(float **in, float **out, unsigned long count);
+    /// Stereo input, stereo output.
+    void run_adding(float **in, float **out, unsigned long count);
+    void set_run_adding_gain(float);
+
+    float delay(void);		// ms
+    float balance(void);
+    float detune(void);		// cents
+    float lpf(void);
+    float lpf_freq(void);	// KHz
+    float predelay(void);	// ms
+
+    // -45.0 to 45.0 ms
+    void set_delay(float);
+    // -1.0 to 1.0
+    void set_balance(float);
+    // -15.0 to 15.0 cents
+    void set_detune(float);
+    // -1.0 to 1.0
+    void set_lpf(float);
+    // 1.0 to 18.0 KHz
+    void set_lpf_freq(float);
+    // 0.0 to 100.0 ms
+    void set_predelay(float);
+};
+
 #endif
